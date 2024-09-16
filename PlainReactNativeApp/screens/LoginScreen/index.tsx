fd80371cfc94e440fc2f8989d74533adf934b312
Antoni Łubisz <lanti222@gmail.com>
2024-09-16 10:13:25 +0200

    fix: Modify keyboard styling

--- PlainReactNativeApp/screens/LoginScreen/index.tsx
+++ PlainReactNativeApp/screens/LoginScreen/index.tsx
@@ -1,5 +1,11 @@
 import React, {useEffect} from 'react';
-import {NativeModules, Text} from 'react-native';
+import {
+  NativeModules,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {styled} from 'nativewind';
 import {LoginForm, LogoImage} from '@/components';
@@ -12,13 +18,17 @@ const LoginScreen = () => {
   const {t} = useTranslation();
 
   return (
-    <StyledSafeAreaView className="flex-1 justify-start items-center bg-darkPurple py-12">
-      <StyledText className="text-slate-200 text-3xl mb-4 font-light">
-        {t('Logowanie')}
-      </StyledText>
-      <LogoImage size={2} classes="mb-24" />
-      <LoginForm />
-    </StyledSafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{flex: 1}}>
+      <StyledSafeAreaView className="flex-1 justify-start items-center bg-darkPurple py-12">
+        <StyledText className="text-slate-200 text-3xl mb-4 font-light">
+          {t('Logowanie')}
+        </StyledText>
+        <LogoImage size={2} classes="mb-24" />
+        <LoginForm />
+      </StyledSafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
